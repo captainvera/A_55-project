93c28c2802ccb11b3b62a2bd7fe08fddb7b54707
Miguel Coimbra Vera <miguel.coimbra.vera@gmail.com>
2016-04-15 22:47:30 +0100

    Added first Broker Unit Tests

---
+++ broker-ws/src/test/java/pt/upa/broker/ws/BrokerUnitTest.java
@@ -0,0 +1,148 @@
+package pt.upa.broker.ws;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.xml.registry.JAXRException;
+
+import mockit.*;
+import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
+import pt.upa.transporter.ws.*;
+import pt.upa.transporter.ws.cli.TransporterClient;
+
+
+public class BrokerUnitTest{
+
+	final static JobView jv1 = new JobView();
+	final static JobView jv2 = new JobView();
+	static BrokerPort broker;
+	static ArrayList<String> _endpoints = new ArrayList<String>();
+	@Mocked UDDINaming uddi;
+	@Mocked TransporterClient client;
+
+	// one-time initialization and clean-up
+	@BeforeClass
+	public static void oneTimeSetUp() {
+		jv1.setCompanyName("UpaTransporter1");
+		String identifier = "Lisboa" + "T" + "Leiria" + "ID" + "1";
+		jv1.setJobIdentifier(identifier);
+		jv1.setJobOrigin("Lisboa");
+		jv1.setJobDestination("Leiria");
+		jv1.setJobPrice(10);
+		jv1.setJobState(JobStateView.PROPOSED);
+
+		jv2.setCompanyName("UpaTransporter1");
+		String identifier1 = "Leiria" + "T" + "Coimbra" + "ID" + "2";
+		jv2.setJobIdentifier(identifier1);
+		jv2.setJobOrigin("Leiria");
+		jv2.setJobDestination("Coimbra");
+		jv2.setJobPrice(15);
+		jv2.setJobState(JobStateView.PROPOSED);
+
+		_endpoints.add("String for first transporter");
+	}
+
+	@AfterClass
+	public static void oneTimeTearDown() {
+
+		_endpoints = null;
+
+	}
+
+	// initialization and clean-up for each test
+	@Before
+	public void setUp() throws Exception {
+
+		new StrictExpectations() {{
+			new UDDINaming(anyString);
+
+			uddi.list("UpaTransporter%"); 
+			result = _endpoints;
+
+			new TransporterClient();
+
+			client.connectToTransporterByURI(anyString);
+			client.ping();
+			result = "UpaTransporter1";
+		}};
+
+		broker = new BrokerPort();
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	@Test
+	public void testtemp(){
+		System.out.println("[TEST] notest ");
+
+	}
+
+	/*
+	 * Testing Exception handling
+	 */
+	@Test(expected=InvalidPriceFault_Exception.class)
+	public void testInvalidPrice() throws Exception{
+		System.out.println("[TEST] Invalid Price ");
+		new StrictExpectations() {{
+			new UDDINaming(anyString);
+
+			uddi.list("UpaTransporter%"); 
+			result = _endpoints;
+
+			new TransporterClient();
+
+			client.connectToTransporterByURI(anyString);
+			client.ping();
+			result = "UpaTransporter1";
+		}};
+		broker.requestTransport("Leiria", "Lisboa", 0);
+	}
+
+	@Test(expected=UnknownLocationFault_Exception.class)
+	public void testInvalidLocation() throws Exception{
+		System.out.println("[TEST] Invalid Location ");
+		new StrictExpectations() {{
+			new UDDINaming(anyString);
+
+			uddi.list("UpaTransporter%"); 
+			result = _endpoints;
+
+			new TransporterClient();
+
+			client.connectToTransporterByURI(anyString);
+			client.ping();
+			result = "UpaTransporter1";
+		}};
+		broker.requestTransport("Munique", "Lisboa", 10);
+	}
+
+	@Test(expected=UnavailableTransportFault_Exception.class)
+	public void testUnavailableTransport() throws Exception{
+		System.out.println("[TEST] Unavailable Transport ");
+		new NonStrictExpectations() {{
+			new UDDINaming(anyString);
+  		
+  		uddi.list("UpaTransporter%"); 
+  		result = _endpoints;
+  		
+  		new TransporterClient();
+  		
+  		client.connectToTransporterByURI(anyString);
+  		client.ping();
+  		result = "UpaTransporter1";
+			client.requestJob("Beja", "Porto", 10);
+			result = new UnavailableTransportFault_Exception("Fabrication", null);
+		}};
+		broker.requestTransport("Beja", "Porto", 10);
+	}
+  
+
+
+}
