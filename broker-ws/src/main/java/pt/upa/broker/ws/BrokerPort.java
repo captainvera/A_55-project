c2cf868fa200f710d536cf882a06199cb6164eaf
Miguel Coimbra Vera <miguel.coimbra.vera@gmail.com>
2016-05-11 01:38:47 +0100

    Added timers and second broker reliability behaviour

--- broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
+++ broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
@@ -32,69 +32,69 @@ serviceName = "BrokerService"
 
 public class BrokerPort implements BrokerPortType{
 
-	TreeMap<String, TransporterClient> transporters = new TreeMap<String, TransporterClient>();
-	ArrayList<Transport> _transports = new ArrayList<Transport>();
-	int _idCounter;
-<<<<<<< HEAD
+  TreeMap<String, TransporterClient> transporters = new TreeMap<String, TransporterClient>();
+  ArrayList<Transport> _transports = new ArrayList<Transport>();
+  int _idCounter;
   private Timer _timer;
-=======
-	BrokerPortType _broker;
-	boolean _primary;
+  BrokerPortType _broker;
+  boolean _primary;
+  private String _url;
 
-	public BrokerPort() throws Exception{
-		connectToTransporters();
-		System.out.println("Broker Initalized.");
+  public BrokerPort() throws Exception{
+    connectToTransporters();
+    System.out.println("Broker Initalized.");
   }
 
   public BrokerPort(boolean primary, String url) throws Exception{
-		_primary = primary;
-		if(primary){
-			connectToTransporters();
-		}
-		else{
-			connectToBroker();
-			_broker.sendInfo(url);
-		}
-		System.out.println("Broker Initalized.");
+    _url = url;
+    _primary = primary;
+    if(primary){
+      connectToTransporters();
+    }
+    else{
+      connectToBroker();
+      _broker.sendInfo(url);
+      stillAlive(10);
+    }
+    System.out.println("Broker Initalized.");
   }
->>>>>>> 290a9b595e67fc4a0456639ff7e0c5ba3829ec11
 
-	public void connectToBroker() throws Exception{
-		String uURL = "http://localhost:9090";
-		UDDINaming uddiNaming = new UDDINaming(uURL);
-		String epAddress = uddiNaming.lookup("UpaBroker");
+  public void connectToBroker() throws Exception{
+    String uURL = "http://localhost:9090";
+    UDDINaming uddiNaming = new UDDINaming(uURL);
+    String epAddress = uddiNaming.lookup("UpaBroker");
 
-		if(epAddress == null){
-			System.out.println("Broker not found");
-			return;
-		}
-		else System.out.printf("Connected to broker @%s%n", epAddress);
+    if(epAddress == null){
+      System.out.println("Broker not found");
+      return;
+    }
+    else System.out.printf("Connected to broker @%s%n", epAddress);
 
-		BrokerService service = new BrokerService();
+    BrokerService service = new BrokerService();
 
-		_broker = service.getBrokerPort();
+    _broker = service.getBrokerPort();
 
-		BindingProvider bindingProvider = (BindingProvider) _broker;
+    BindingProvider bindingProvider = (BindingProvider) _broker;
 
-		Map<String, Object> requestContext = bindingProvider.getRequestContext();
-		requestContext.put(ENDPOINT_ADDRESS_PROPERTY, epAddress);
-	}
+    Map<String, Object> requestContext = bindingProvider.getRequestContext();
+    requestContext.put(ENDPOINT_ADDRESS_PROPERTY, epAddress);
+  }
 
-	public void connectToBrokerByURI(String endp) {
-		try{
-    	BrokerPortType port = null;
-    	BrokerService service = new BrokerService();
-    	port = service.getBrokerPort();
-    	System.out.println("Setting endpoint address ...");
-    	BindingProvider bindingProvider = (BindingProvider) port;
-    	Map<String, Object> requestContext = bindingProvider.getRequestContext();
-    	requestContext.put(ENDPOINT_ADDRESS_PROPERTY, endp);
-    	System.out.println("Connection succesful to " + endp);
-    	_broker = port;
-		} catch(Exception e){
-			System.out.printf("Caught exception: %s%n", e);
-			e.printStackTrace();
-		}
+  public void connectToBrokerByURI(String endp) {
+    try{
+      BrokerPortType port = null;
+      BrokerService service = new BrokerService();
+      port = service.getBrokerPort();
+      System.out.println("Setting endpoint address ...");
+      BindingProvider bindingProvider = (BindingProvider) port;
+      Map<String, Object> requestContext = bindingProvider.getRequestContext();
+      requestContext.put(ENDPOINT_ADDRESS_PROPERTY, endp);
+      System.out.println("Connection succesful to " + endp);
+      _broker = port;
+    } catch(Exception e){
+      System.out.printf("Caught exception: %s%n", e);
+      e.printStackTrace();
+    }
   }
 
   private void connectToTransporters() throws JAXRException{
@@ -117,83 +117,83 @@ public class BrokerPort implements BrokerPortType{
         name = client.ping();
         transporters.put(name,client);
         System.out.println("Transporter: " + name + " was added to broker");
-        } catch(Exception e){e.printStackTrace();}
-		}
+      } catch(Exception e){e.printStackTrace();}
+    }
 
-	}
+  }
 
-	public String ping(String name){
-		String res = new String();
-		System.out.printf("Pinging all transporters%n");
-		for (Map.Entry<String, TransporterClient> entry : transporters.entrySet()){
-			entry.getValue().ping();
-		}
-		return "Contact has been established with " + transporters.size() + " Transporters.\n";
-	}
+  public String ping(String name){
+    String res = new String();
+    System.out.printf("Pinging all transporters%n");
+    for (Map.Entry<String, TransporterClient> entry : transporters.entrySet()){
+      entry.getValue().ping();
+    }
+    return "Contact has been established with " + transporters.size() + " Transporters.\n";
+  }
 
-	protected Transport getTransportById(String id){
-		for(Transport t : _transports){
-			if(t.getId().equals(id)){
-				return t;
-			}
-		}
-		return null;
-	}
+  protected Transport getTransportById(String id){
+    for(Transport t : _transports){
+      if(t.getId().equals(id)){
+        return t;
+      }
+    }
+    return null;
+  }
 
 
-	public TransportView viewTransport(String id) throws UnknownTransportFault_Exception{
-		Transport tv = getTransportById(id);
-		if(tv == null){
+  public TransportView viewTransport(String id) throws UnknownTransportFault_Exception{
+    Transport tv = getTransportById(id);
+    if(tv == null){
       UnknownTransportFault utf = new UnknownTransportFault();
       utf.setId(id);
       throw new UnknownTransportFault_Exception("Transport with " + id + " not found", utf);
     }
-		String name = tv.getTransporterCompany();
-		TransporterClient tpt = transporters.get(name);
-		JobStateView state = tpt.jobStatus(id).getJobState();
-		switch (state) {
-			case HEADING:
-			tv.setState(TransportStateView.HEADING);
-			break;
+    String name = tv.getTransporterCompany();
+    TransporterClient tpt = transporters.get(name);
+    JobStateView state = tpt.jobStatus(id).getJobState();
+    switch (state) {
+      case HEADING:
+        tv.setState(TransportStateView.HEADING);
+        break;
 
-			case ONGOING:
-			tv.setState(TransportStateView.ONGOING);
-			break;
+      case ONGOING:
+        tv.setState(TransportStateView.ONGOING);
+        break;
 
-			case COMPLETED:
-			tv.setState(TransportStateView.COMPLETED);
-			break;
+      case COMPLETED:
+        tv.setState(TransportStateView.COMPLETED);
+        break;
 
-			default:
-			break;
-		}
+      default:
+        break;
+    }
 
-		return tv.toTransportView();
-	}
+    return tv.toTransportView();
+  }
 
-	public void clearTransports(){
-		int i = 0;
-		System.out.printf("Executing order 66.%n");
-		for (Map.Entry<String, TransporterClient> entry : transporters.entrySet()){
-			entry.getValue().clearJobs();
-			System.out.printf("Order 66 on transporter %d%n", i+1);
-			i++;
-		}
+  public void clearTransports(){
+    int i = 0;
+    System.out.printf("Executing order 66.%n");
+    for (Map.Entry<String, TransporterClient> entry : transporters.entrySet()){
+      entry.getValue().clearJobs();
+      System.out.printf("Order 66 on transporter %d%n", i+1);
+      i++;
+    }
     _transports.clear();
-		System.out.println("The Sith Shall Rule again.");
-	}
+    System.out.println("The Sith Shall Rule again.");
+  }
 
-	public List<TransportView> listTransports(){
+  public List<TransportView> listTransports(){
     ArrayList<TransportView> _tviews = new ArrayList<TransportView>();
     for(Transport t : _transports){
       _tviews.add(t.toTransportView());
     }
     return _tviews;
-	}
+  }
 
-	public void checkBestTransporter (ArrayList<JobView> jobs, Transport transport){
-		int min = 101;
-		JobView bestJob = null;
+  public void checkBestTransporter (ArrayList<JobView> jobs, Transport transport){
+    int min = 101;
+    JobView bestJob = null;
     for(JobView job : jobs){
       if(job.getJobPrice() < min){
         transport.setTransporterCompany(job.getCompanyName());
@@ -204,11 +204,11 @@ public class BrokerPort implements BrokerPortType{
     }
   }
 
-	public void confirmJob(ArrayList<JobView> jobs, Transport transport){
-		for (JobView job : jobs){
-			try{
-				if (transport.getTransporterCompany().equals(job.getCompanyName()))
-				transporters.get(job.getCompanyName()).decideJob(job.getJobIdentifier(), true);
+  public void confirmJob(ArrayList<JobView> jobs, Transport transport){
+    for (JobView job : jobs){
+      try{
+        if (transport.getTransporterCompany().equals(job.getCompanyName()))
+          transporters.get(job.getCompanyName()).decideJob(job.getJobIdentifier(), true);
 
         else transporters.get(job.getCompanyName()).decideJob(job.getJobIdentifier(), false);
 
@@ -251,103 +251,110 @@ public class BrokerPort implements BrokerPortType{
   }
 
 
-	public String requestTransport(String origin, String destination, int priceMax)
-	throws InvalidPriceFault_Exception, UnavailableTransportFault_Exception,
-	UnavailableTransportPriceFault_Exception, UnknownLocationFault_Exception{
-    transporters.clear();
-    try{
-      connectToTransporters();
-    } catch(JAXRException e) {e.printStackTrace();}
+  public String requestTransport(String origin, String destination, int priceMax)
+    throws InvalidPriceFault_Exception, UnavailableTransportFault_Exception,
+                    UnavailableTransportPriceFault_Exception, UnknownLocationFault_Exception{
+             transporters.clear();
+             try{
+               connectToTransporters();
+             } catch(JAXRException e) {e.printStackTrace();}
 
-    Location l_origin = Location.fromValue(origin);
-    Location l_destination = Location.fromValue(destination);
+             Location l_origin = Location.fromValue(origin);
+             Location l_destination = Location.fromValue(destination);
 
-    if(l_origin == null){
-      UnknownLocationFault ulf = new UnknownLocationFault();
-      ulf.setLocation(origin);
-      throw new UnknownLocationFault_Exception("Invalid origin:", ulf);
-    }
+             if(l_origin == null){
+               UnknownLocationFault ulf = new UnknownLocationFault();
+               ulf.setLocation(origin);
+               throw new UnknownLocationFault_Exception("Invalid origin:", ulf);
+             }
 
-    if(l_destination == null){
-      UnknownLocationFault ulf = new UnknownLocationFault();
-      ulf.setLocation(destination);
-      throw new UnknownLocationFault_Exception("Invalid destination:", ulf);
-    }
+             if(l_destination == null){
+               UnknownLocationFault ulf = new UnknownLocationFault();
+               ulf.setLocation(destination);
+               throw new UnknownLocationFault_Exception("Invalid destination:", ulf);
+             }
 
-    if(priceMax <= 0) {
-      InvalidPriceFault ipf = new InvalidPriceFault();
-      ipf.setPrice(priceMax);
-      throw new InvalidPriceFault_Exception("Invalid price:", ipf);
-    }
+             if(priceMax <= 0) {
+               InvalidPriceFault ipf = new InvalidPriceFault();
+               ipf.setPrice(priceMax);
+               throw new InvalidPriceFault_Exception("Invalid price:", ipf);
+             }
 
-		Transport newTransport = new Transport(origin, destination, priceMax, TransportStateView.REQUESTED);
+             Transport newTransport = new Transport(origin, destination, priceMax, TransportStateView.REQUESTED);
 
-    ArrayList<JobView> _jobs = makeRequests(origin, destination, priceMax);
+             ArrayList<JobView> _jobs = makeRequests(origin, destination, priceMax);
 
-    if(_jobs.isEmpty()){
-			newTransport.setState(TransportStateView.FAILED);
-      UnavailableTransportFault utf = new UnavailableTransportFault();
-      utf.setOrigin(origin);
-      utf.setDestination(destination);
-			throw new UnavailableTransportFault_Exception("No Transport Available", utf);
-		}
+             if(_jobs.isEmpty()){
+               newTransport.setState(TransportStateView.FAILED);
+               UnavailableTransportFault utf = new UnavailableTransportFault();
+               utf.setOrigin(origin);
+               utf.setDestination(destination);
+               throw new UnavailableTransportFault_Exception("No Transport Available", utf);
+             }
 
-  	checkBestTransporter(_jobs, newTransport);
+             checkBestTransporter(_jobs, newTransport);
 
-		newTransport.setState(TransportStateView.BUDGETED);
+             newTransport.setState(TransportStateView.BUDGETED);
 
-		if(newTransport.getPrice() > priceMax){
-			rejectAllOptions(_jobs);
-			newTransport.setState(TransportStateView.FAILED);
-      UnavailableTransportPriceFault utpf = new UnavailableTransportPriceFault();
-      utpf.setBestPriceFound(newTransport.getPrice());
-			throw new UnavailableTransportPriceFault_Exception("Price above maximum given by client", utpf);
-		}
+             if(newTransport.getPrice() > priceMax){
+               rejectAllOptions(_jobs);
+               newTransport.setState(TransportStateView.FAILED);
+               UnavailableTransportPriceFault utpf = new UnavailableTransportPriceFault();
+               utpf.setBestPriceFound(newTransport.getPrice());
+               throw new UnavailableTransportPriceFault_Exception("Price above maximum given by client", utpf);
+             }
 
-		else{
-			confirmJob(_jobs, newTransport);
-			newTransport.setState(TransportStateView.BOOKED);
-		}
-		_transports.add(newTransport);
-		return newTransport.getId();
-	}
+             else{
+               confirmJob(_jobs, newTransport);
+               newTransport.setState(TransportStateView.BOOKED);
+             }
+             _transports.add(newTransport);
+             return newTransport.getId();
+  }
 
-  protected void stillAlive(){
-    TimerTask timerTask = new TimerTask() {
+  protected void stillAlive(int time) throws Exception{
+    TimerTask _timerTask = new TimerTask() {
       @Override
       public void run() {
-        System.out.println("STILL ALIVE and kicking");
-        String ret = _broker.ping();
-        if(ret == null){
-          GYST();
+        try{
+          _broker.ping("ping");
+          System.out.println("STILL ALIVE and kicking");
+          stillAlive(10);
+        } catch (Exception e) {
+          try{
+            GYST(); 
+          } catch (Exception eg){}
         }
       }
     };
-    if(timer != null)
-      timer.cancel();
-    timer = new Timer();
-    timer.schedule(timerTask, 10000);
+    if(_timer != null)
+      _timer.cancel();
+    _timer = new Timer();
+    _timer.schedule(_timerTask, time*1000);
   }
 
-  private void GYST(){
-  
-    //TODO 
-  
-  
+  private void GYST() throws Exception{
+
+    System.out.println("ASSUMING CONTROL"); 
+    connectToTransporters();
+    System.out.println("All your base are belong to us");
+    String uURL = "http://localhost:9090";
+    UDDINaming uddiNaming = new UDDINaming(uURL);
+    uddiNaming.rebind("UpaBroker", _url);
   }
 
-	public void update(TransportView transport) {
-		Transport transportToUpdate = getTransportById(transport.getId());
-		if (transportToUpdate == null) {
-			transportToUpdate = new Transport(transport);
-			_transports.add(transportToUpdate);
-			return ;
-		}
-		transportToUpdate.setState(transport.getState());
-	}
+  public void update(TransportView transport) {
+    Transport transportToUpdate = getTransportById(transport.getId());
+    if (transportToUpdate == null) {
+      transportToUpdate = new Transport(transport);
+      _transports.add(transportToUpdate);
+      return ;
+    }
+    transportToUpdate.setState(transport.getState());
+  }
 
-	public void sendInfo(String url){
-		connectToBrokerByURI(url);
-	}
+  public void sendInfo(String url){
+    connectToBrokerByURI(url);
+  }
 
 }
